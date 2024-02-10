bbfc40940856f765f296f782cfd7d70f971093ed
alternoegraha <noegrahachan@gmail.com>
2024-02-10 07:22:50 +0700

    adapt to pixelOS

--- start.sh
+++ start.sh
@@ -4,26 +4,29 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # init
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 
 # Run inside foss.crave.io devspace
 # Remove existing local_manifests
-crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
+crave run --clean --no-patch -- "rm -rf * .repo && \
+
+# init PixelOS
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs && \
 
 # sync repo
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
 # sync tree
-git clone -b 13-staging https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
+git clone -b 14-staging https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
 
 # clone hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi && \
+git clone https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi && \
 
 # Set up build environment
 source build/envsetup.sh && \
 
 # Lunch configuration
-lunch lineage_fog-userdebug && \
+lunch aosp_fog-userdebug && \
 
 # Build the ROM
 mka bacon"
