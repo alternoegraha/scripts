12e636b8d44c9f56f1674e9f0e865186b43bd8a8
alternoegraha <noegrahachan@gmail.com>
2024-02-15 06:21:45 +0700

    21

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # init
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 
 # Run inside foss.crave.io devspace
 # Remove existing local_manifests
@@ -14,10 +14,10 @@ crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
 # sync tree
-git clone -b wip/round-corner-experimentations https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
+git clone -b lineage-21 https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
 
 # clone hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi && \
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi && \
 
 # Set up build environment
 source build/envsetup.sh && \
