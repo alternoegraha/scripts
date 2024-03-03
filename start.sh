210fb4563894d1806caaf63ecb9435def1206e38
alternoegraha <noegrahachan@gmail.com>
2024-03-03 14:41:37 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # init
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init --depth 1 -u https://github.com/DerpFest-AOSP/manifest.git -b 13
 
 # Run inside foss.crave.io devspace
 # Remove existing local_manifests
@@ -14,22 +14,22 @@ crave run --no-patch --projectID=72 -- "rm -rf .repo .repo/local_manifests andro
 export TZ='Asia/Jakarta' && \
 
 # init EvoX
-repo init --depth=1 -u https://github.com/Evolution-X/manifest -b udc && \
+repo init --depth=1 -u https://github.com/DerpFest-AOSP/manifest.git -b 14 && \
 
 # sync repo
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
 # sync tree
-git clone -b evox-14 https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
+git clone -b derpfest-14 https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
 
 # Set up build environment
 source build/envsetup.sh && \
 
 # Lunch configuration
-lunch evolution_fog-userdebug && \
+lunch derp_fog-userdebug && \
 
 # Build the ROM
-m evolution"
+m derp"
 
 # Pull generated zip files
 crave pull out/target/product/*/*.zip --projectID=72
