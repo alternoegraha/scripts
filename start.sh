e9584a7bcb32a94ddd168905b2293a3d5a8436cb
alternoegraha <noegrahachan@gmail.com>
2024-04-25 06:34:59 +0700

    evoxyz

--- start.sh
+++ start.sh
@@ -4,32 +4,32 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # init
-repo init --depth 1 -u https://github.com/DerpFest-AOSP/manifest.git -b 13
+repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 # Run inside foss.crave.io devspace
 # Remove existing local_manifests
-crave run --no-patch --projectID=64 -- "rm -rf .repo .repo/local_manifests prebuilts && \
+crave run --no-patch --projectID=72 -- "rm -rf .repo .repo/local_manifests prebuilts && \
 
 # set timezone
 export TZ='Asia/Jakarta' && \
 
 # init EvoX
-repo init --depth=1 -u https://github.com/Evolution-X/manifest -b udc && \
+repo init --depth=1 -u https://github.com/Evolution-XYZ/manifest -b udc && \
 
 # sync repo
-repo sync -c -j\$(nproc --all) --force-sync --prune --no-clone-bundle --no-tags && \ 
+bash /opt/crave/resync.sh && \
 
 # sync tree
-git clone -b evox-14 https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
+git clone -b evox-yz https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
 
 # Set up build environment
 source build/envsetup.sh && \
 
 # Lunch configuration
-lunch evolution_fog-eng && \
+lunch lineage_fog-userdebug && \
 
 # Build the ROM
 m evolution -j\$(nproc --all)"
 
 # Pull generated zip files
-crave pull out/target/product/*/*.zip --projectID=64
+crave pull out/target/product/*/*.zip --projectID=72
