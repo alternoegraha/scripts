3854d61218faf90972a6222ba578a6bfa790c272
alternoegraha <noegrahachan@gmail.com>
2024-04-29 17:48:03 +0700

    huh

--- start.sh
+++ start.sh
@@ -2,17 +2,23 @@
 set -e
 # Run inside foss.crave.io devspace
 crave run --no-patch -- "rm -rf .repo .repo/local_manifests && \
+
 # set timezone
 export TZ='Asia/Jakarta' && \
+
 # sync repo
-/opt/crave/resync.sh && \ 
+/opt/crave/resync.sh && \
+
 # sync tree
 git clone -b fourteen-qpr2 https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
+
 # Set up
 export BUILD_USERNAME=alternoegraha && \
 export BUILD_HOSTNAME=nurture && \
 source build/envsetup.sh && \
+
 # Lunch configuration
 lunch aosp_fog-ap1a-userdebug && \
-# Build the ROM
+
+# Build
 mka bacon -j\$(nproc --all)"
