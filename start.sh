b6845ca9fd39715cbccbca17e2c6c6fb69049506
alternoegraha <noegrahachan@gmail.com>
2024-11-24 09:51:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,14 @@ set -e
 
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 
+# remove my tree first so they can reclone
+rm -rf device/xiaomi/fog && \
+
 # clone local manifests
 git clone https://github.com/alternoegraha/local_manifest.git .repo/local_manifests && \
 
 # resync repo
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+repo sync -c -j20 --force-sync --no-clone-bundle --no-tags && \ 
 
 # Set up build environment
 source build/envsetup.sh && \
