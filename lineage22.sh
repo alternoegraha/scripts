f01e4a907eb83d866d276b8015d8a6e50f5e5346
An. <noegrahachan@gmail.com>
2024-11-25 07:25:05 +0700

    Create lineage22.sh

---
+++ lineage22.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+crave run --no-patch -- "rm -rf .repo .repo/local_manifests && \
+
+# init LOS22
+repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-22.0 && \
+
+# sync + fixup repo
+bash /opt/crave/resync.sh && \
+
+# clone device tree
+git clone -b lineage-22 https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
+
+# Set up build environment
+source build/envsetup.sh && \
+
+# Lunch configuration
+breakfast fog userdebug && \
+
+# Build the ROM
+mka bacon"
+
+# Pull generated zip files
+crave pull out/target/product/fog/*.zip
